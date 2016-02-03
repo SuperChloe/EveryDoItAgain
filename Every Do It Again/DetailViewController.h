5f74858d4af5595ef6e57c4de22e42f1e11417e8
Chloe Horgan <super.chloe@gmail.com>
2016-02-03 11:29:10 -0500

    Initial Commit

---
+++ Every Do It Again/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Every Do It Again
+//
+//  Created by Chloe on 2016-02-03.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Every Do It Again/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  Every Do It Again
+//
+//  Created by Chloe on 2016-02-03.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
